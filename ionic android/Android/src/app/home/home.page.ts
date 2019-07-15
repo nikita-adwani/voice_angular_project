5dbb3e91ed12ef64ce0cd65ec9fa4cb20c642c2a
nikita-adwani <niknikkynikita@gmail.com>
2019-07-15 17:48:42 +0530

    server ,js

--- ionic android/Android/src/app/home/home.page.ts
+++ ionic android/Android/src/app/home/home.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { DataServiceService } from "src/app/services/dataService.service";
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+  constructor(public dataService: DataServiceService) {}
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.getStudentDetials();
+  }
 
-  constructor() {}
-
+  getStudentDetials() {
+    const input = "Aakash";
+    this.dataService.getStudentDetails(input).subscribe(res => {
+      console.log(res);
+    });
+  }
 }
---
+++ ionic android/dataserver/server.js
@@ -0,0 +1,47 @@
+var http = require("http");
+const fs = require("fs");
+
+let studentsData = [];
+fs.readFile("./studentData.json", (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    let rawdata = data;
+    studentsData = JSON.parse(rawdata);
+});
+//create a server object:
+http
+    .createServer(function(req, res) {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+        res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+        res.setHeader(
+            "Access-Control-Allow-Headers",
+            "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+        );
+
+        let input = (req.url.split("/"))[1];
+
+        console.log(input, req.url)
+        const studentData = studentsData.getStudentDetail;
+        studentData.filter(function(item) {
+            if (item.FirstName === input) {
+                let returnObj = {
+                    contactNumber: item.StudentsContactNo,
+                    dob: item.dob,
+                    father: item.Fathers_name,
+                    fatherContactNumber: item.FathersContactNo,
+                    firstName: item.FirstName,
+                    gender: item.Gender,
+                    id: item.MasterId,
+                    imageName: item.ImgName,
+                    lastName: item.Last_name,
+                    year: item.Year
+                }
+                res.write(JSON.stringify(returnObj));
+            }
+        });
+        res.end(); //end the response
+    })
+    .listen(8081); //the server object listens on port 8080
