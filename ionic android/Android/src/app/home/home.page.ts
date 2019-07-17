bc132954a3cd4fee72a24061016ea5041daee1f6
sona323 <ysonakshi57@gmail.com>
2019-07-17 15:08:06 +0530

    updates by suhasee AND sonakshi

--- ionic android/Android/src/app/home/home.page.ts
+++ ionic android/Android/src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, ViewChild } from "@angular/core";
 import { DataServiceService } from "src/app/services/dataService.service";
 
 import { Animation } from "@ionic/core";
@@ -11,72 +11,94 @@ import { Animation } from "@ionic/core";
 export class HomePage {
   window: any = window;
   webkitSpeechRecognition: any;
+  mappedData: any;
+  public intentTypes: any;
 
-  public intentTypes:any;
-
-  resultDiv = document.getElementById("result");
+  @ViewChild("resultDiv") resultDiv: ElementRef;
 
   constructor(public dataService: DataServiceService) {
     this.intentTypes = [
-        {
-          intent: "helloIntent",
-          input: [
-            "Hi",
-            "hi",
-            "hello",
-            "hola",
-            "hey",
-            "namaste",
-            "salam",
-            "salaam",
-            "bonjour",
-            "konichiwa",
-            "konnichiwa",
-            "ola"
-          ],
-          response: "Hello! How can i help you?"
-        },
-        {
-          intent: "byeIntent",
-          input: ["Bye", "bye", "see you", "tata", "see you", "goodbye"],
-          response: "Bye!, take care, have a nice day."
-        },
-        {
-          intent: "detailIntent",
-          input: ["give", "details", "student", "named", "what", "name", "who"],
-          response: "Give me a second. I think you are looking for "
-        }
-        
-      ]
-      console.log(this.intentTypes)
-
+      {
+        intent: "helloIntent",
+        input: [
+          "Hi",
+          "hi",
+          "hello",
+          "hola",
+          "hey",
+          "namaste",
+          "salam",
+          "salaam",
+          "bonjour",
+          "konichiwa",
+          "konnichiwa",
+          "ola"
+        ],
+        response: "Hello! How can i help you?"
+      },
+      {
+        intent: "byeIntent",
+        input: ["Bye", "bye", "see you", "tata", "see you", "goodbye"],
+        response: "Bye!, take care, have a nice day."
+      },
+      {
+        intent: "detailIntent",
+        input: ["give", "details", "student", "named", "what", "name", "who"],
+        response: "Give me a second. I think you are looking for "
+      }
+    ];
+    console.log(this.intentTypes);
   }
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    this.getStudentDetials();
+    // this.getStudentDetials();
   }
 
-  getStudentDetials() {
-    const input = "Nikita";
-    this.dataService.getStudentDetails(input).subscribe(res => {
+  getStudentDetails(studentName) {
+    // console.log(input);
+    let speech = "";
+    this.dataService.getStudentDetails(studentName).subscribe(res => {
       console.log(res);
+
+      if (res.length > 1) {
+       speech = "I found " + res.length + " students named " + studentName;
+        this.saySomething(speech);
+
+        res.forEach(item => {
+          let speech = item.firstName + " " + item.lastName + ",";
+          this.saySomething(speech);
+        });
+      }
+
+      if (res.length === 1) {
+       speech =
+          "I found 1 student named " +
+          studentName +
+          ", " +
+          res[0].firstName +
+          " " +
+          res[0].lastName;
+        this.saySomething(speech);
+      }
+
+      if (res.length === 0) {
+         speech =
+          "I did not find any students with the name "+ studentName +", please try again.";
+        this.saySomething(speech);
+      }
     });
   }
 
   startConversing() {
+    let classThis = this;
 
-    
-      let classThis = this;
-      
     // 'speak.this("Hii, I am Databot . I am here to help you. you can talk to me now.")';
     // saySomething.this.speak("Hii, I am Databot . I am here to help you. you can talk to me now.");
 
-      
     if ("webkitSpeechRecognition" in window) {
-      
       // speak("Hii, I am Databot . I am here to help you. you can talk to me now.");
-      
+
       var speechRecognizer = new webkitSpeechRecognition();
       // classThis.saySomething.speak("hiii how is uu????");
       speechRecognizer.continuous = true; //Controls whether continuous results are returned for each recognition, or only a single result.
@@ -100,7 +122,7 @@ export class HomePage {
           console.log("inputWords", inputWords);
 
           let foundIntent = [];
-          console.log(classThis.intentTypes)
+          console.log(classThis.intentTypes);
           foundIntent = classThis.getIntent(inputWords, classThis.intentTypes);
           console.log(foundIntent);
 
@@ -115,7 +137,15 @@ export class HomePage {
               replyIntent[0].intent === "detailIntent"
             ) {
               studentName = classThis.getStudentName(inputWords);
-              // ---- check yesterday code
+
+              classThis.getStudentDetails(studentName);
+              //get details from some api for student
+              //  this.dataServices.getStudentName(studentName).subscribe(input=>{
+              //   classThis.saySomething(input);
+              //   console.log(input);
+
+              //   })
+
               let text = "";
             }
 
@@ -130,7 +160,7 @@ export class HomePage {
             interimTranscripts += transcript;
           }
         }
-        this.resultDiv.innerHTML =
+        classThis.resultDiv.nativeElement.innerHTML =
           finalTranscripts +
           '<span style="color:#999">' +
           interimTranscripts +
@@ -139,14 +169,14 @@ export class HomePage {
 
       speechRecognizer.onerror = function(event) {};
     } else {
-      this.resultDiv.innerHTML =
+      classThis.resultDiv.nativeElement.innerHTML =
         "Your browser is not supported. If google chrome, please upgrade!";
     }
   }
 
   saySomething(speechresult) {
     let msg = new SpeechSynthesisUtterance(speechresult);
-   
+
     setTimeout(() => {
       console.log(window.speechSynthesis.getVoices());
 
@@ -160,7 +190,7 @@ export class HomePage {
 
       console.log(speechresult);
       window.speechSynthesis.speak(msg);
-    }, 500);
+    }, 0);
   }
 
   getIntent(inputWords, intentTypesArray) {
